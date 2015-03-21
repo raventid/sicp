9f68a8994e14505f90e2fe1e095c480058071a08
raventid <juliankul@gmail.com>
2015-03-21 15:57:19 +0300

    add comment about execution

--- fast-expt.scm
+++ fast-expt.scm
@@ -3,7 +3,9 @@
  
 (define (even? n)
   (= (remainder n 2) 0)) 
-  
+ 
+;repl.it can't invoke remainder operation have to set up emacs with mit-scheme:)
+
 (define (fast-expt b n)
     (cond ((= n 0) 1)
           ((even? n) (square (fast-expt (/ n 2)))

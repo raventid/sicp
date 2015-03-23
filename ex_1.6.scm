26df7472e60c86670b9f44f3bd8a0537b1158001
raventid <juliankul@gmail.com>
2015-03-23 23:43:34 +0300

    add ex 1.16

---
+++ ex_1.6.scm
@@ -0,0 +1,19 @@
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+ 
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+
+;sqrt-iter will never terminate.
+;The first time new-if is called it is passed two arguments, guess and (sqrt-iter (improve guess x) x).
+;Scheme uses applicative order evaluation and so another call to sqrt-iter will be evaluated resulting in the following behaviour which won’t terminate:
+;sqrt-iter
+;  new-if
+;    sqrt-iter
+;      new-if 
+;        ...

4e6817da55f95b10275d3b1dc70f20464c157b50
raventid <juliankul@gmail.com>
2015-03-25 23:19:32 +0300

    update ex 1.17

--- chapter1/ex_1.17.scm
+++ chapter1/ex_1.17.scm
@@ -6,12 +6,23 @@ Assume double and halve are defined by the language
   
  (define (* a b) 
       (cond ((= b 0) 0) 
-	             ((even? b) (double (* a (halve b)))) 
+	             ((even?  (double (* a (halve b)))) 
 		              (else (+ a (* a (- b 1)))))) 
   
- ;; Testing 
- ;;  (* 2 4) 
- ;;   (* 4 0) 
- ;;    (* 5 1) 
- ;;     (* 7 10) 
- 
+; Testing 
+;  (* 2 4) 
+;  (* 4 0) 
+;  (* 5 1) 
+;  (* 7 10)     
+;
+;using tail recursion achieves better space order of growth of theta(1)
+
+ (define (fast-mult-by-add a b) 
+   (define (double x) (+ x x)) 
+   (define (halve x) (/ x 2)) 
+    
+   (define (helper a b product) ;; "add a" b times 
+     (cond ((= b 0) product) 
+           ((even? b) (helper (double a) (halve b) product)) 
+           (else (helper a (- b 1) (+ a product))))) 
+   (helper a b 0)) 

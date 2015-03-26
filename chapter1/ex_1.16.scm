60e93f512fbc16654b474db3fb2d91bdacc145dc
raventid <raventid@users.noreply.github.com>
2015-03-26 19:27:51 +0300

    Update ex_1.16.scm

--- chapter1/ex_1.16.scm
+++ chapter1/ex_1.16.scm
@@ -6,6 +6,9 @@
            ((even? n) (iter a (square b) (/ n 2))) 
            (else (iter (* a b) b (- n 1))))) 
    (iter 1 b n)) 
+   
+ (define (even? n)
+    (= (remainder n 2) 0))  
   
  (define (square x) (* x x)) 
   

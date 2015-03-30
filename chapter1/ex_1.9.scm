0bef72f618fa2145d31978ecd763e873ab117286
raventid <juliankul@gmail.com>
2015-03-31 00:03:39 +0300

    add ex 1.9

---
+++ chapter1/ex_1.9.scm
@@ -0,0 +1,15 @@
+; (+ 4 5) 
+; (inc (+ (dec 4) 5)) 
+; (inc (+ 3 5)) 
+; (inc (inc (+ (dec 3) 5))) 
+; (inc (inc (+ 2 5))) 
+; (inc (inc (inc (+ (dec 2) 5)))) 
+; (inc (inc (inc (+ 1 5)))) 
+; (inc (inc (inc (inc (+ (dec 1) 5))))) 
+; (inc (inc (inc (inc (+ 0 5))))) 
+; (inc (inc (inc (inc 5)))) 
+; (inc (inc (inc 6))) 
+; (inc (inc 7)) 
+; (inc 8) 
+  
+; 9 

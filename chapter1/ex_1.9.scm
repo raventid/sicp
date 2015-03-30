c3763b0035b1dfe9611f071285f4605d1ec18b7e
raventid <juliankul@gmail.com>
2015-03-31 00:07:16 +0300

    update ex 1.9

--- chapter1/ex_1.9.scm
+++ chapter1/ex_1.9.scm
@@ -14,4 +14,24 @@
 ; (inc (inc 7)) 
 ; (inc 8) 
   
+; 9
+;
+;
+;
+;
+;
+;
+;
+; Other execution is iterative
+;
+; (+ 4 5) 
+; (+ (dec 4) (inc 5)) 
+; (+ 3 6) 
+; (+ (dec 3) (inc 6)) 
+; (+ 2 7) 
+; (+ (dec 2) (inc 7)) 
+; (+ 1 8) 
+; (+ (dec 1) (inc 8)) 
+; (+ 0 9) 
+  
 ; 9 

c054f8361a5198a9826aea1fde7ec42635b080db
raventid <juliankul@gmail.com>
2015-10-27 10:53:12 -0400

    little update of ex 1.8

--- chapter1/ex_1.8.scm
+++ chapter1/ex_1.8.scm
@@ -1,15 +1,16 @@
+;; ex 1.8
+
 (define (cube-root x)
   (cube-root-iter 1.0 0 x))
- 
+
 (define (cube-root-iter guess last-guess x)
   (if (good-enough? guess last-guess x)
       guess
       (cube-root-iter (improve-cube guess x)
                       guess
                       x)))
- 
+
 (define (improve-cube guess x)
   (/ (+ (/ x (square guess))
         (* 2 guess))
      3))
-

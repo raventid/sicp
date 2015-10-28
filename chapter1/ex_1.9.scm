3a7bece4061dc0d7b3ff55ec59b8b38f1f204363
raventid <juliankul@gmail.com>
2015-10-28 18:21:53 -0400

    little update of ex 1.9

--- chapter1/ex_1.9.scm
+++ chapter1/ex_1.9.scm
@@ -1,3 +1,4 @@
+;; ex 1.9
 ; Execution of first procedure uses recursion
 ;
 ;
@@ -7,20 +8,20 @@
       (inc (+ (dec a) b))))
 ;
 ;
-; (+ 4 5) 
-; (inc (+ (dec 4) 5)) 
-; (inc (+ 3 5)) 
-; (inc (inc (+ (dec 3) 5))) 
-; (inc (inc (+ 2 5))) 
-; (inc (inc (inc (+ (dec 2) 5)))) 
-; (inc (inc (inc (+ 1 5)))) 
-; (inc (inc (inc (inc (+ (dec 1) 5))))) 
-; (inc (inc (inc (inc (+ 0 5))))) 
-; (inc (inc (inc (inc 5)))) 
-; (inc (inc (inc 6))) 
-; (inc (inc 7)) 
-; (inc 8) 
-  
+; (+ 4 5)
+; (inc (+ (dec 4) 5))
+; (inc (+ 3 5))
+; (inc (inc (+ (dec 3) 5)))
+; (inc (inc (+ 2 5)))
+; (inc (inc (inc (+ (dec 2) 5))))
+; (inc (inc (inc (+ 1 5))))
+; (inc (inc (inc (inc (+ (dec 1) 5)))))
+; (inc (inc (inc (inc (+ 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+
 ; 9
 ;
 ;
@@ -34,14 +35,14 @@
 ;
 ;
 ;
-; (+ 4 5) 
-; (+ (dec 4) (inc 5)) 
-; (+ 3 6) 
-; (+ (dec 3) (inc 6)) 
-; (+ 2 7) 
-; (+ (dec 2) (inc 7)) 
-; (+ 1 8) 
-; (+ (dec 1) (inc 8)) 
-; (+ 0 9) 
-  
-; 9 
+; (+ 4 5)
+; (+ (dec 4) (inc 5))
+; (+ 3 6)
+; (+ (dec 3) (inc 6))
+; (+ 2 7)
+; (+ (dec 2) (inc 7))
+; (+ 1 8)
+; (+ (dec 1) (inc 8))
+; (+ 0 9)
+
+; 9
